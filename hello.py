d810b1f9b7d2b5e0cdbdf0927d1ff58aebd3d4e5
pabhila <abhilash1.p@intel.com>
2023-05-16 17:35:52 +0530

    Test

--- hello.py
+++ hello.py
@@ -1,1 +1,24 @@
-print("Hello World")
+import os
+import json
+
+#Pass the above JSON file and create a file object
+
+fileObj = open('BuildConfig.json',)
+
+#Convert JSON and parse as Python Dictionary
+
+jsonDict = json.load(fileObj)
+
+print(jsonDict)
+
+#Substitute the value wherever required using os.path.expandvars
+KlocworkProject = os.getenv("KlocworkProject")
+KlocworkCSharpProject = os.getenv("KlocworkCSharpProject")
+CoverityStream = os.getenv("CoverityStream")
+CoverityCSharpStream = os.getenv("CoverityCSharpStream")
+
+print("KlocworkProject: ", KlocworkProject)
+print("KlocworkCSharpProject: ", KlocworkCSharpProject)
+print("CoverityStream: ", CoverityStream)
+print("CoverityCSharpStream: ", CoverityCSharpStream)
+
